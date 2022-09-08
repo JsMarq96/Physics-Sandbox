68e9b7a20b6931849efed168741ae22a77057ca7
Juan S. Marquerie <juansebastian.marquerie01@estudiant.upf.edu>
2022-09-08 21:30:36 +0200

    Added some physics infrastrucutre

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <cstddef>
+#include <cstdint>
 #include <iostream>
 #include <GL/gl3w.h>
 #include <GLFW/glfw3.h>
@@ -8,12 +10,53 @@
 
 #include "mesh.h"
 #include "mesh_renderer.h"
+#include "trasform.h"
 
 #define WIN_WIDTH	740
 #define WIN_HEIGHT	680
 #define WIN_NAME	"Physics-Sandbox"
 
+#define INSTANCE_COUNT 50
+
 void temp_error_callback(int error_code, const char* descr);
+
+
+enum eShape : uint8_t {
+   SPHERE = 0,
+   PLANE,
+   BOX,
+   SHAPE_COUNT
+};
+
+sTransform transforms[INSTANCE_COUNT];
+sSpeed speeds[INSTANCE_COUNT];
+bool is_static[INSTANCE_COUNT];
+eShape shapes[INSTANCE_COUNT];
+float mass[INSTANCE_COUNT];
+float inv_mass[INSTANCE_COUNT];
+uint16_t obj_count = 0;
+
+
+inline uint16_t add_sphere(const glm::vec3 &position,
+                           const float radius,
+                           const float mass_i,
+                           const bool is_static_i) {
+    transforms[obj_count].position = position;
+    transforms[obj_count].scale = {radius, radius, radius};
+    shapes[obj_count] = SPHERE;
+    is_static[obj_count] = is_static_i;
+
+    if (is_static_i) {
+        inv_mass[obj_count] = 0.0f;
+        mass[obj_count] = 0.0f;
+    } else {
+        inv_mass[obj_count] = 1.0f / mass_i;
+        mass[obj_count] = mass_i;
+    }
+
+    return obj_count++;
+}
+
 
 void main_loop(GLFWwindow *window) {
     glfwMakeContextCurrent(window);
@@ -29,6 +72,15 @@ void main_loop(GLFWwindow *window) {
     sphere.clean();
     cube.clean();
 
+    memset(is_static, false, sizeof(is_static));
+
+
+    uint16_t sphere_obj = add_sphere({0.0f, 50.0f, 0.0f},
+                                     1.0f,
+                                     10.0f,
+                                     false);
+
+    double start_time = glfwGetTime(), prev_frame_time;
     while(!glfwWindowShouldClose(window)) {
         int width, heigth;
         double temp_mouse_x, temp_mouse_y;
@@ -41,12 +93,42 @@ void main_loop(GLFWwindow *window) {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
 
+        double curr_frame_time = glfwGetTime();
+        double elapsed_time = curr_frame_time - prev_frame_time;
+        prev_frame_time = curr_frame_time;
+
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
         ImGui::Begin("Test");
         ImGui::End();
+
+
+        // Update
+        for(uint16_t i = 0; i < obj_count; i++) {
+            if (!is_static[i]) {
+                continue;
+            }
+            // Add gravity
+            speeds[i].linear += {0.0f, -9.8f, 0.0f};
+        }
+
+
+        // Integrate
+        for(uint16_t i = 0; i < obj_count; i++) {
+            if (!is_static[i]) {
+                continue;
+            }
+
+            transforms[i].position += speeds[i].linear * elapsed_time;
+            // TODO: angular speed integration
+
+            // Add some energy loss
+            speeds[i].linear *= 0.999f;
+            speeds[i].angular *= 0.999f;
+        }
+
 
         ImGui::Render();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
